4b76f8267f7c0956f3e2725501e17fe198dc8ed1
Paito Anderson <pj.paito@gmail.com>
2023-12-12 13:17:22 -0500

    Use _host variable

--- src/GraphQLClientLibrary/Services/GraphQLClientService.cs
+++ src/GraphQLClientLibrary/Services/GraphQLClientService.cs
@@ -21,29 +21,26 @@ namespace GraphQLClientLibrary.Services
 
     public class GraphQLClientService : IGraphQLClientService
     {
-        public GraphQLClientService()
-        {
-        }
+        private static string _host = "https://app.load1.com";
 
         public async Task<string> GetBearerToken(string clientId, string secret)
         {
             HttpClient client = new HttpClient();
-            string uri = "https://app.load1.com/api/auth/token";
             var authenticateRequest = new AuthTokenRequest
             {
                 ClientID = clientId,
                 ClientSecret = secret
             };
             var content = new StringContent(JsonConvert.SerializeObject(authenticateRequest), Encoding.UTF8, "application/json");
-            var response = await client.PostAsync(uri, content);
+            var response = await client.PostAsync($"{_host}/api/auth/token", content);
             response.EnsureSuccessStatusCode();
             var authTokenResponse = JsonConvert.DeserializeObject<AuthTokenResponse>(await response.Content.ReadAsStringAsync());
             return authTokenResponse.AccessToken;
         }
 
         public async Task<List<Vehicle>> QueryViaGraphQLClientIntegration(string bearerToken, GraphQLRequest queryRequest)
-        {          
-            var graphQLClient = new GraphQLClient("https://app.load1.com/api/graphql/");
+        {
+            var graphQLClient = new GraphQLClient($"{_host}/api/graphql/");
             graphQLClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {bearerToken}");
             var graphQLResponse = await graphQLClient.PostAsync(queryRequest);
 
@@ -60,17 +57,16 @@ namespace GraphQLClientLibrary.Services
         }
 
         public async Task<List<Vehicle>> QueryManually(string bearerToken, string queryRequest)
-        {           
+        {
             return await QueryRequest(bearerToken, queryRequest);
         }
 
         public async Task<List<Vehicle>> QueryRequest(string bearerToken, string query)//, VehicleQuery queryRequest)
         {
             HttpClient client = new HttpClient();
-            string uri = "https://app.load1.com/api/graphql";
             client.DefaultRequestHeaders.Add("Authorization", $"Bearer {bearerToken}");
             var content = new StringContent(query, Encoding.UTF8, "application/graphql-query");
-            var response = await client.PostAsync(uri, content);
+            var response = await client.PostAsync($"{_host}/api/graphql", content);
             JObject jObj = JObject.Parse(await response.Content.ReadAsStringAsync());
 
             if (response.IsSuccessStatusCode)
